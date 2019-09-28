d0a0b84d27f4fe8521f8d593c2e445f1338e6c5c
Regan <regan.simmonds@uoit.net>
2019-09-27 21:24:13 -0400

    Moving Unity Project into Repo

---
+++ PluginTest/Assets/MovementScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+
+public class MovementScript : MonoBehaviour
+{
+
+    //define the dll
+    const string DLL_NAME = "PLUGINSOLUTION";
+    [DllImport(DLL_NAME)]
+
+    //SAVING FUNCTION SAVES THE POSITION OF THE CUBE, SPECIFICALLY X, Y, Z
+    private static extern void savingFunction(string nameFile, float x, float y, float z);
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MOVEMENT, LEFT, RIGHT AND FORWARD
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward;
+        }
+      
+
+        //SAVING POSITION
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 position = transform.position;
+            savingFunction("HOWDY.txt", position.x, position.y, position.z);
+
+        }
+        //LOADING POSITION
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+
+        }
+
+    }
+}
